aad26aba95f1a503912e322870dd0c63c1d08c79
Ritul <rituls619@gmail.com>
2024-01-13 13:02:58 +0100

    db init & close func

--- back-end/db/db.go
+++ back-end/db/db.go
@@ -1,5 +1,33 @@
 package database
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Db_client *mongo.Client
+
 func Init_db() error {
 
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	var err error
+	Db_client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func Close_db() {
+	err := Db_client.Disconnect(context.TODO())
+	if err != nil {
+		panic(err)
+	}
 }
--- back-end/main.go
+++ back-end/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	database "example.com/StuDuwo/back-end/db"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -16,4 +18,27 @@ func main() {
 
 	app.Listen(":3000")
 
+}
+
+func init_App() error {
+
+	err := loadEnv()
+	if err != nil {
+		return err
+	}
+
+	err = database.Init_db()
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func loadEnv() error {
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+	return nil
 }
