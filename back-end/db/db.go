a9dfe68518f06cd1f9a650ab246f53e484c13a87
Ritul <rituls619@gmail.com>
2024-01-14 23:52:53 +0100

    started get and post

--- back-end/db/db.go
+++ back-end/db/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
@@ -11,17 +12,23 @@ import (
 
 var Db_client *mongo.Client
 
+func Get_Collection(name string) *mongo.Collection {
+	return Db_client.Database("rental_db").Collection(name)
+}
+
 func Init_db() error {
 
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+		log.Fatal("You must set your 'MONGODB_URI' environment variable.")
 	}
 	var err error
 	Db_client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Connected to MongoDB!")
+
 	return nil
 }
 
--- back-end/main.go
+++ back-end/main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
+	"context"
+
 	database "example.com/StuDuwo/back-end/db"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 )
+
+type Rental_post struct {
+	House_name  string `json:"house_name"`
+	Location    string `json:"location"`
+	Start_date  string `json:"start_date"`
+	Rent        int    `json:"rent"`
+	Description string `json:"description"`
+	Image_URL   string `json:"image_url"`
+}
+
+func get_all_rentals(c *fiber.Ctx) error {
+	return nil
+}
+
+func post_new_rental(c *fiber.Ctx) error {
+	sample_post := bson.M{"house_name": "Uilenstede",
+		"location":    "Amstelveen",
+		"start_date":  "Feb 1",
+		"rent":        "500",
+		"description": "Nice house",
+		"image_url":   "dummy_url"}
+
+	// if err := c.BodyParser(new_post); err != nil {
+	// 	return err
+	// }
+	collection := database.Get_Collection("rentals")
+
+	_, err := collection.InsertOne(context.TODO(), sample_post)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Error inserting new post")
+	}
+	return nil
+}
 
 func main() {
 
-	//fmt.Println("Hello world")
+	err := init_App()
+	if err != nil {
+		panic(err)
+	}
 
 	app := fiber.New()
 
@@ -21,12 +60,13 @@ func main() {
 }
 
 func init_App() error {
-
+	// Setup env
 	err := loadEnv()
 	if err != nil {
 		return err
 	}
 
+	// Initialize DB
 	err = database.Init_db()
 	if err != nil {
 		return err
